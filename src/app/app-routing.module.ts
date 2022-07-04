12b6117a6612f62c6d060aacfd367f504c3835b7
leiah8 <leiah.nay8@gmail.com>
2022-07-04 13:51:20 -0400

    basic menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ContainerComponent } from './container/container.component';
+import { MoonfactoryComponent } from './moonfactory/moonfactory.component';
+import { PlanetfactoryComponent } from './planetfactory/planetfactory.component';
+import { Moongame1Component } from './moongame1/moongame1.component';
+
 
 
 const routes: Routes = [
   { path: 'renderer', component: ContainerComponent },
+  { path: 'moonFactory', component: MoonfactoryComponent },
+  { path: 'planetFactory', component: PlanetfactoryComponent },
+  { path: 'moonGame', component: Moongame1Component }
 ]
 
 @NgModule({
