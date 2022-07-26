a7cd660ade9d2d815ad4d8be3ce83c1e8765af47
leiah8 <leiah.nay8@gmail.com>
2022-07-26 12:13:25 -0400

    add new decompose component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { MoonfactoryComponent } from './moonfactory/moonfactory.component';
 import { PlanetfactoryComponent } from './planetfactory/planetfactory.component';
 import { KhDecomposeNumberComponent } from './kh-decompose-number/kh-decompose-number.component';
 import { DecomposeAsteroidsComponent } from './decompose-asteroids/decompose-asteroids.component';
+import { DecomposeToPlanetComponent } from './decompose-to-planet/decompose-to-planet.component';
 import { Moongame1Component } from './moongame1/moongame1.component';
 
 
@@ -13,8 +14,9 @@ const routes: Routes = [
   { path: 'moonGame', component: ContainerComponent },
   { path: 'moonFactory', component: MoonfactoryComponent },
   { path: 'planetFactory', component: PlanetfactoryComponent },
-  { path: 'decompose', component: KhDecomposeNumberComponent },
-  { path: 'asteroids', component: DecomposeAsteroidsComponent },
+  { path: 'decomposePlanets', component: KhDecomposeNumberComponent },
+  { path: 'decomposeAsteroids', component: DecomposeAsteroidsComponent },
+  { path: 'decompose', component: DecomposeToPlanetComponent },
   //{ path: 'moonGame', component: Moongame1Component }
 ]
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { Moongame1Component } from './moongame1/moongame1.component';
 import { PlanetfactoryComponent } from './planetfactory/planetfactory.component';
 import { KhDecomposeNumberComponent } from './kh-decompose-number/kh-decompose-number.component';
 import { DecomposeAsteroidsComponent } from './decompose-asteroids/decompose-asteroids.component';
+import { DecomposeToPlanetComponent } from './decompose-to-planet/decompose-to-planet.component';
 
 @NgModule({
   declarations: [
@@ -21,6 +22,7 @@ import { DecomposeAsteroidsComponent } from './decompose-asteroids/decompose-ast
     PlanetfactoryComponent,
     KhDecomposeNumberComponent,
     DecomposeAsteroidsComponent,
+    DecomposeToPlanetComponent,
   ],
   imports: [
     BrowserModule,
